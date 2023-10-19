3d87d397266e7dbcdda2d3d54b8f68ef5da3cce8
Mohamed Nasser <142774751+MohamedNasser101@users.noreply.github.com>
2023-10-19 18:08:55 +0300

    Create Calculate_the_Nth_term.c

---
+++ C/Calculate_the_Nth_term.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+// Calculate the Nth term.
+
+int s(int n, int a, int b, int c)
+{
+    if(n==1)
+    {
+        return a;
+    }
+    else if(n==2)
+    {
+        return b;
+    }
+    else if(n==3)
+    {
+        return c;
+    }
+    else
+    {
+        return s(n - 1, a, b, c)+s(n - 2, a, b, c)+s(n - 3, a, b, c);
+    }
+
+}
+
+int main() {
+    int n, a, b, c;
+
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = s(n, a, b, c);
+
+    printf("%d", ans);
+    return 0;
+}
